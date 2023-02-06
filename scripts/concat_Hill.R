01243183e8485cee3f8f01b3a055c99dffa57255
Eirik Høye <eirikhoye@users.noreply.github.com>
2023-02-06 11:13:03 +0100

    Update concat_Hill.R

--- scripts/concat_Hill.R
+++ scripts/concat_Hill.R
@@ -1,7 +1,8 @@
 suppressPackageStartupMessages(library(tidyverse))
 
 """
-Script for merging all Hill diversity and evenness profiles into tidy dataframe
+Script for merging all Hill diversity and evenness profiles 
+into tidy dataframe, and calculate area under the curve (AUC)
 """
 
 # Get paths from snakemake script
@@ -9,13 +10,13 @@ hill_out <- snakemake@output[['hill_out']]
 auc_out <- snakemake@output[['auc_out']]
 file_paths <- snakemake@input[['in_path']]
 
-### CONCATENATE ALL HILL DIVERSITIES ###
+# Concatenate all Hill profile dataframes
 df <- file_paths %>%
     map(read_tsv, col_types=cols()) %>%
     reduce(rbind)
 write_tsv(df, file = hill_out)
 
-#### MAKE AUC CURVES ###
+# Make AUC curves
 library(Bolstad2)
 samples <- unique(df$sample_id)
 auc_df <- matrix(ncol=1, nrow=0)
@@ -27,6 +28,7 @@ for (i in samples){
   auc_df[i] <- auc
 }
 
+# Write AUC curve dataframe
 auc_df <- data.frame(auc_df)
 auc_df['sample_id'] <- rownames(auc_df)
 auc_df['clonality'] = 10 - auc_df['auc_df']
