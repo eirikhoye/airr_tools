a12d6032cfeb9568c188cbc0cf752c21dd082312
Eirik Høye <eirikhoye@users.noreply.github.com>
2023-02-06 15:39:40 +0100

    Add files via upload

---
+++ scripts/global_params.py
@@ -0,0 +1,107 @@
+import pandas as pd
+import numpy as np
+import igraph as ig
+import sys
+
+filepath = sys.argv[1]
+outpath = sys.argv[2]
+
+################################################################################################
+
+                           # Define functions
+
+################################################################################################
+
+def global_params(g):
+	global_param = {
+	'nr_nodes' : len(g.vs),
+	'nr_edges' : len(g.es),
+	'largest_component' : max([len(i) for i in g.clusters()]),
+	'maximal_k_core' : max(g.coreness()),
+	'diameter' : g.diameter(),
+	'assortativity' : g.assortativity_degree(directed=False),
+	'average_cluster' : np.mean([len(i) for i in g.clusters()]),
+	'average_degree' : np.mean(sorted(g.indegree(), reverse=True)),
+	'clustering_coeff' : np.mean(g.transitivity_local_undirected(mode='zero')),
+	'density' : np.mean(g.density(loops=False))
+	}
+
+	global_param = pd.DataFrame(global_param, index=['parameter'])
+	global_param = global_param.transpose()
+	return global_param
+
+
+#################################################################################################
+
+                           # Load Graph
+
+#################################################################################################
+
+g = ig.Graph.Read_GraphML(filepath)
+
+params = (global_params(g))
+params.to_csv(sys.argv[2], sep='\t')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ scripts/local_params.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+import igraph as ig
+import sys
+
+filepath = sys.argv[1]
+outpath = sys.argv[2]
+
+################################################################################################
+
+                           # Define functions
+
+################################################################################################
+
+def local_params(g):
+	local_param = {
+	'transitivity' : np.mean(g.transitivity_local_undirected(mode='zero')),
+	'authority' : np.mean(g.authority_score()),
+	'pagerang' : np.mean(g.personalized_pagerank(directed=False)),
+	'eigenvector_centrality' : np.mean(g.eigenvector_centrality(directed=False)),
+	'closeness' : np.mean(g.closeness()),
+	'betweenness' : np.mean(g.betweenness(directed=False))
+	}
+
+	local_param = pd.DataFrame(local_param, index=['parameter'])
+	local_param = local_param.transpose()
+	return local_param
+
+
+#################################################################################################
+
+                           # Load Graph
+
+#################################################################################################
+
+g = ig.Graph.Read_GraphML(filepath)
+
+params = (local_params(g))
+params.to_csv(sys.argv[2], sep='\t')
+
+
+
+
+
