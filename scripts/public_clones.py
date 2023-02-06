a183a3ba68838f60e2abb21d90783cccfdd3a8d3
Eirik Høye <eirikhoye@users.noreply.github.com>
2023-02-06 12:01:42 +0100

    Update public_clones.py

--- scripts/public_clones.py
+++ scripts/public_clones.py
@@ -3,12 +3,15 @@ import pandas as pd
 import numpy as np
 import os
 
+"""
+Script for calculating clonal sharing level (number of patients where
+the clone was detected).
 
-#########################################################################################################
+To use, change the paths for input files to paths on your system.
 
-# Define functions
-
-#########################################################################################################
+Files are located in goolge drive folder linked to in the repository
+README.md
+"""
 
 def get_public_clones(airr_dict):
     """
@@ -107,12 +110,12 @@ def get_public_clones_sharing_level(airr_dict):
 # meta_dict = meta['COMET_ID_group'].to_dict()
 
 
-meta_path1 = '/Users/eirikhoy/Dropbox/projects/airr_comet/data/immunarch_data_376276/metadata.txt'
+meta_path1 = '/path/to/data/immunarch_data_376276/metadata.txt'
 meta1 = pd.read_csv(meta_path1, sep='\t')
 meta1['COMET_ID_group'] = meta1['COMET_ID'] + '_group:_' + meta1['group']
 meta1.index = meta1['Sample']
 
-meta_path2 = '/Users/eirikhoy/Dropbox/projects/airr_comet/data/immunarch_data_12511620/metadata.txt'
+meta_path2 = '/path/to/data/immunarch_data_12511620/metadata.txt'
 meta2 = pd.read_csv(meta_path2, sep='\t')
 meta2['COMET_ID_group'] = meta2['COMET_ID'] + '_group:_' + meta2['group']
 meta2.index = meta2['Sample']
@@ -130,7 +133,7 @@ meta_dict = meta['COMET_ID_group'].to_dict()
 # as keys and the dataframe as value pair. Input for get_public_clones function.
 
 airr_dict = {}
-file_dir = '/Users/eirikhoy/Dropbox/projects/airr_comet/data/immunarch_data_376276/'
+file_dir = '/path/to/data/immunarch_data_376276/'
 
 for file in os.listdir(file_dir):
     path = file_dir + file
@@ -142,7 +145,7 @@ for file in os.listdir(file_dir):
     sample_name = meta_dict[file.split('.')[-2]]
     airr_dict[sample_name] = pd.read_csv(path, sep='\t')
 
-file_dir = '/Users/eirikhoy/Dropbox/projects/airr_comet/data/immunarch_data_12511620/'
+file_dir = '/path/to/data/immunarch_data_12511620/'
 
 for file in os.listdir(file_dir):
     path = file_dir + file
@@ -161,8 +164,8 @@ for file in os.listdir(file_dir):
 
 #########################################################################################################
 
-out_file_path = '/Users/eirikhoy/Dropbox/projects/airr_comet/data/public_clones.txt'
-out_file_path2 = '/Users/eirikhoy/Dropbox/projects/airr_comet/data/public_clones_sharing_level.csv'
+out_file_path = '/path/to/data/public_clones.txt'
+out_file_path2 = '/path/to/data/public_clones_sharing_level.csv'
 
 remove_samples = [
     'COMET-0011M1_2_group:_no_chemo',
@@ -185,7 +188,7 @@ airr_dict_sub = {key: airr_dict[key] for key in airr_dict.keys() if key not in r
 public_clones_sharing_level = get_public_clones_sharing_level(airr_dict_sub)
 public_clones_sharing_level.to_csv(out_file_path2, index=False)
 
-out_dir_public = '/Users/eirikhoy/Dropbox/projects/airr_comet/data/rearrangement_files_with_sharing_public/'
+out_dir_public = '/path/to/data/rearrangement_files_with_sharing_public/'
 for k, v in airr_dict_sub.items():
     public_df = pd.merge(left=v, right=public_clones_sharing_level,
                          left_on='amino_acid', right_on='amino_acid',
@@ -193,73 +196,17 @@ for k, v in airr_dict_sub.items():
     public_df = public_df.sort_values(by='templates', ascending=False)
     public_df.to_csv(str(out_dir_public + k + '_public.tsv'), sep='\t')
 
-out_dir_private = '/Users/eirikhoy/Dropbox/projects/airr_comet/data/rearrangement_files_with_sharing_private/'
+out_dir_private = '/path/to/data/rearrangement_files_with_sharing_private/'
 for k, v in airr_dict_sub.items():
     private_df = v[~v['amino_acid'].isin(public_clones_sharing_level['amino_acid'].values)]
     private_df['sharing_level'] = 1
     private_df = private_df.sort_values(by='templates', ascending=False)
     private_df.to_csv(str(out_dir_private + k + '_private.tsv'), sep='\t')
 
-out_dir_all = '/Users/eirikhoy/Dropbox/projects/airr_comet/data/rearrangement_files_with_sharing_all/'
+out_dir_all = '/path/to/data/rearrangement_files_with_sharing_all/'
 for k, v in airr_dict_sub.items():
     all_df = pd.merge(left=v, right=public_clones_sharing_level,
                       left_on='amino_acid', right_on='amino_acid',
                       how='left').fillna({'sharing_level': 1})
     all_df = all_df.sort_values(by='templates', ascending=False)
     all_df.to_csv(str(out_dir_all + k + '_all.tsv'), sep='\t')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
