e435684bcefa30ed6f34b9e74d0d57138a916c2b
Eirik Høye <eirikhoye@users.noreply.github.com>
2023-02-06 11:02:09 +0100

    Update and rename combine_cdr3_files.py to concatenate_rearrangement_files.py

--- scripts/concatenate_rearrangement_files.py
+++ scripts/concatenate_rearrangement_files.py
@@ -2,6 +2,12 @@ import pandas as pd
 import sys
 import os
 
+"""
+Script to concatenate rearrangement files, 
+
+Usage: python combine_cdr3_files.py <rearrangement_file_1.tsv> <rearrangement_file_2.tsv> <rearrangement_file_[...].tsv>
+"""
+
 path_list = sys.argv[1:-1]
 
 is_first = True
